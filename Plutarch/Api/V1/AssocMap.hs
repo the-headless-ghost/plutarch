6494ec95e99d9ad15bcea93cee6cc267d9475d85
Peter Dragos <peter@mlabs.city>
2022-02-27 11:18:05 -0500

    add type equality constraint to PUnsafeLiftDecl PMap

--- Plutarch/Api/V1/AssocMap.hs
+++ Plutarch/Api/V1/AssocMap.hs
@@ -1,50 +1,52 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
-module Plutarch.Api.V1.AssocMap (
-  PMap (PMap),
-) where
-
-import qualified Plutus.V1.Ledger.Api as Plutus
-import qualified PlutusTx.AssocMap as PlutusMap
+module Plutarch.Api.V1.AssocMap
+  ( PMap (PMap),
+  )
+where
 
 import Plutarch.Builtin (PBuiltinMap)
-import Plutarch.Lift (
-  PConstantRepr,
-  PConstanted,
-  PLifted,
-  PUnsafeLiftDecl,
-  pconstantFromRepr,
-  pconstantToRepr,
- )
+import Plutarch.Lift
+  ( PConstantRepr,
+    PConstanted,
+    PLifted,
+    PUnsafeLiftDecl,
+    pconstantFromRepr,
+    pconstantToRepr,
+  )
 import Plutarch.Prelude
+import qualified Plutus.V1.Ledger.Api as Plutus
+import qualified PlutusTx.AssocMap as PlutusMap
 
 newtype PMap (k :: PType) (v :: PType) (s :: S) = PMap (Term s (PBuiltinMap k v))
   deriving (PlutusType, PIsData) via (DerivePNewtype (PMap k v) (PBuiltinMap k v))
 
 instance
-  ( Plutus.ToData (PLifted v)
-  , Plutus.ToData (PLifted k)
-  , Plutus.FromData (PLifted v)
-  , Plutus.FromData (PLifted k)
-  , PLift k
-  , PLift v
+  ( PConstanted (PLifted k) ~ k,
+    PConstanted (PLifted v) ~ v,
+    Plutus.ToData (PLifted v),
+    Plutus.ToData (PLifted k),
+    Plutus.FromData (PLifted v),
+    Plutus.FromData (PLifted k),
+    PLift k,
+    PLift v
   ) =>
   PUnsafeLiftDecl (PMap k v)
   where
   type PLifted (PMap k v) = PlutusMap.Map (PLifted k) (PLifted v)
 
 instance
-  ( PLifted (PConstanted k) ~ k
-  , Plutus.ToData v
-  , Plutus.FromData v
-  , Plutus.ToData k
-  , Plutus.FromData k
-  , PConstant k
-  , PLifted (PConstanted v) ~ v
-  , Plutus.FromData v
-  , Plutus.ToData v
-  , PConstant v
+  ( PLifted (PConstanted k) ~ k,
+    Plutus.ToData v,
+    Plutus.FromData v,
+    Plutus.ToData k,
+    Plutus.FromData k,
+    PConstant k,
+    PLifted (PConstanted v) ~ v,
+    Plutus.FromData v,
+    Plutus.ToData v,
+    PConstant v
   ) =>
   PConstant (PlutusMap.Map k v)
   where
