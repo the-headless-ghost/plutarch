8c428b7911df594133e329f631f7922dbeeb007b
Emily Martins <emi@haskell.fyi>
2022-04-14 20:21:46 +0200

    add `PAssetClass` versions of `PTagged` conversions

--- plutarch-safemoney/src/Plutarch/SafeMoney.hs
+++ plutarch-safemoney/src/Plutarch/SafeMoney.hs
@@ -25,7 +25,9 @@ module Plutarch.SafeMoney (
 
   -- * Conversions
   pdiscreteValue,
+  pdiscreteValue',
   pvalueDiscrete,
+  pvalueDiscrete',
 ) where
 
 import Prelude hiding (Num (..))
@@ -40,9 +42,9 @@ import Plutarch.Prelude
 
 --------------------------------------------------------------------------------
 
-import Plutarch.Api.V1.Extra (passetClass, passetClassValueOf, psingletonValue)
+import Plutarch.Api.V1.Extra (PAssetClass, passetClass, passetClassValue, passetClassValueOf, psingletonValue)
 
-import Plutarch.SafeMoney.Tagged (PTagged (PTagged), Tagged (Tagged))
+import Plutarch.SafeMoney.Tagged (PTagged, Tagged (Tagged))
 import Plutarch.SafeMoney.Tagged qualified as Tagged
 
 type PDiscrete tag = PTagged tag PInteger
@@ -50,17 +52,28 @@ type PDense tag = PTagged tag PRational
 
 --------------------------------------------------------------------------------
 
-{- | Downcast a 'PValue' to a 'PDiscrete' unit, providing a witness of the 'AssetClass'.
+{- | Downcast a 'PValue' to a 'PDiscrete' unit, providing a witness of the 'PAssetClass'.
 
      @since 0.3
 -}
 pvalueDiscrete ::
   forall (tag :: Type) (s :: S).
+  Term s (PAssetClass :--> PValue :--> PDiscrete tag)
+pvalueDiscrete = phoistAcyclic $
+  plam $ \ac f ->
+    Tagged.ptag $ passetClassValueOf # f # ac
+
+{- | Downcast a 'PValue' to a 'PDiscrete' unit, providing a witness of the 'AssetClass', which gets inlined. If you use this 'AssetClass' twice, prefer 'pvalueDiscrete'.
+
+     @since 0.3
+-}
+pvalueDiscrete' ::
+  forall (tag :: Type) (s :: S).
   Tagged tag AssetClass ->
   Term s (PValue :--> PDiscrete tag)
-pvalueDiscrete (Tagged (AssetClass (cs, tn))) = phoistAcyclic $
+pvalueDiscrete' (Tagged (AssetClass (cs, tn))) = phoistAcyclic $
   plam $ \f ->
-    pcon . PTagged $ passetClassValueOf # f #$ passetClass # pconstant cs # pconstant tn
+    Tagged.ptag $ passetClassValueOf # f #$ passetClass # pconstant cs # pconstant tn
 
 {- | Get a 'PValue' from a 'PDiscrete', providing a witness of the 'AssetClass'.
      __NOTE__: `pdiscreteValue` after `pvalueDiscrete` is not a round-trip.
@@ -70,12 +83,28 @@ pvalueDiscrete (Tagged (AssetClass (cs, tn))) = phoistAcyclic $
 -}
 pdiscreteValue ::
   forall (tag :: Type) (s :: S).
+  Term s (PAssetClass :--> PDiscrete tag :--> PValue)
+pdiscreteValue = phoistAcyclic $
+  plam $ \ac p ->
+    passetClassValue
+      # ac
+      # Tagged.puntag p
+
+{- | Get a 'PValue' from a 'PDiscrete', providing a witness of the 'AssetClass'.
+     __NOTE__: `pdiscreteValue` after `pvalueDiscrete` is not a round-trip.
+     It filters for a particular tag.
+
+     If you use this 'AssetClass' twice, prefer 'pvalueDiscrete'.
+
+     @since 0.3
+-}
+pdiscreteValue' ::
+  forall (tag :: Type) (s :: S).
   Tagged tag AssetClass ->
   Term s (PDiscrete tag :--> PValue)
-pdiscreteValue (Tagged (AssetClass (cs, tn))) = phoistAcyclic $
-  plam $ \f -> pmatch f $ \case
-    PTagged p ->
-      psingletonValue
-        # pconstant cs
-        # pconstant tn
-        # p
+pdiscreteValue' (Tagged (AssetClass (cs, tn))) = phoistAcyclic $
+  plam $ \p ->
+    psingletonValue
+      # pconstant cs
+      # pconstant tn
+      # Tagged.puntag p
